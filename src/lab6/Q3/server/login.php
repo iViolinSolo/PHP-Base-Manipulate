4bcd885e3d6018488c463da7acf69da36f2d846a
ViolinSolo <i.violinsolo@gmail.com>
2017-10-01 12:57:54 +0800

    [Quiz] add lab6 q3 login

--- src/lab6/Q3/server/login.php
+++ src/lab6/Q3/server/login.php
@@ -5,3 +5,35 @@
  * Date: 01/10/2017
  * Time: 12:36 PM
  */
+
+$filename = '../database/users.txt';
+
+$inputname = $_POST['user_name'];
+$inputpsd = $_POST['user_psd'];
+
+
+$exist = 0;
+$targetpsd = '';
+
+foreach (file($filename) as $line) {
+    list($username, $userpassword) = explode(',', $line);
+
+    if ($username == $inputname) {
+        $exist = 1;
+        $targetpsd = $userpassword;
+        break;
+    }
+}
+
+if ($exist == 0) {
+    echo "<p>User name $inputname not exists in dbs, choose another one!</p>
+<p>Login Fail!</p>";
+} else {
+
+    if ($targetpsd == $inputpsd."\n") {
+        echo "<p>Login success!</p>";
+    } else {
+        echo "Password not matched!";
+    }
+
+}
